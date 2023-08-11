69a277bfc1700b3c0e2b2e5e415d80c146532c0f
Zihao Guo <zihao@codon-login-02.ebi.ac.uk>
2023-08-11 14:07:40 +0100

    :fire:collected_del_oldP2

--- compProject_2_P2/Folder_3addInfo/bash_statMissingID.sh
+++ compProject_2_P2/Folder_3addInfo/bash_statMissingID.sh
@@ -2,11 +2,11 @@
 
 echo "================ Stat for annot ================"
 
-cat /nfs/research/goldman/zihao/Datas/p2_comp_viridian/Folder_3_mergeINFO/Folder_missingData/missing_ann_ids_*.txt | sort | uniq > /nfs/research/goldman/zihao/Datas/p2_comp_viridian/Folder_3_mergeINFO/missing_ann_ids.txt && echo -n "The number of missing IDs for Annot is: " && wc -l /nfs/research/goldman/zihao/Datas/p2_comp_viridian/Folder_3_mergeINFO/missing_ann_ids.txt
+cat /nfs/research/goldman/zihao/Datas/p2_compViridian_P2/Folder_3_mergeINFO/folderData_missingData/missing_ann_ids_*.txt | sort | uniq > /nfs/research/goldman/zihao/Datas/p2_compViridian_P2/Folder_3_mergeINFO/missing_ann_ids.txt && echo -n "The number of missing IDs for Annot is: " && wc -l /nfs/research/goldman/zihao/Datas/p2_comp_viridian/Folder_3_mergeINFO/missing_ann_ids.txt
 
 echo "================ Stat for cov ================"
 
-cat /nfs/research/goldman/zihao/Datas/p2_comp_viridian/Folder_3_mergeINFO/Folder_missingData/missing_cov_ids_*.txt | sort | uniq > /nfs/research/goldman/zihao/Datas/p2_comp_viridian/Folder_3_mergeINFO/missing_cov_ids.txt && echo -n "The number of missing IDs for COV is: " && wc -l /nfs/research/goldman/zihao/Datas/p2_comp_viridian/Folder_3_mergeINFO/missing_cov_ids.txt
+cat /nfs/research/goldman/zihao/Datas/p2_compViridian_P2/Folder_3_mergeINFO/folderData_missingData/missing_cov_ids_*.txt | sort | uniq > /nfs/research/goldman/zihao/Datas/p2_compViridian_P2/Folder_3_mergeINFO/missing_cov_ids.txt && echo -n "The number of missing IDs for COV is: " && wc -l /nfs/research/goldman/zihao/Datas/p2_comp_viridian/Folder_3_mergeINFO/missing_cov_ids.txt
 
 echo "================ Stat total ================"
-echo "The total number of missing IDs is: $(( $(wc -l < /nfs/research/goldman/zihao/Datas/p2_comp_viridian/Folder_3_mergeINFO/missing_ann_ids.txt) + $(wc -l < /nfs/research/goldman/zihao/Datas/p2_comp_viridian/Folder_3_mergeINFO/missing_cov_ids.txt) ))"
+echo "The total number of missing IDs is: $(( $(wc -l < /nfs/research/goldman/zihao/Datas/p2_compViridian_P2/Folder_3_mergeINFO/missing_ann_ids.txt) + $(wc -l < /nfs/research/goldman/zihao/Datas/p2_compViridian_P2/Folder_3_mergeINFO/missing_cov_ids.txt) ))"
